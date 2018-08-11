2e3978a77b7cb46aac5407f157a30972f058bd14
胡鹏 <64hp@163.com>
2018-08-11 18:57:54 +0800

    add

--- NJMediator_NJSisPlayPage/Classes/NJMediator_NJSisPlayPage.swift
+++ NJMediator_NJSisPlayPage/Classes/NJMediator_NJSisPlayPage.swift
@@ -9,11 +9,12 @@ fileprivate let kMediator_Action_NativeFetchSisPlayController = "SisPlayControll
 
 extension NJMediator {
 
-    public func Mediator_NJSisPlayPage_PlayController(video: String, topicId: String) -> UIViewController? {
+    public func Mediator_NJSisPlayPage_PlayController(video: String, topicId: String, videoSize: CGSize) -> UIViewController? {
         
         var params = [String: Any]()
         params["video"] = video
         params["topicId"] = topicId
+        params["videoSize"] = videoSize
         
         let result = self.perform(nameSpace: kMediator_Target_NameSpace, target: kMediator_Target_NJSisPlayPage, action: kMediator_Action_NativeFetchSisPlayController, params: params, shouldCacheTarget: false)
 
